24367c8fc2bd20c6d0c200333260e5491b7c82e6
sasha4ka <sasha4ka99991@gmail.com>
2026-04-21 21:10:37 +0300

    add app.crud.user get_user_by_email()

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,14 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from passlib.context import CryptContext
+from app.core.password import hash_password
 from app.db.models.user import User
 from app.schemas.user import User_Read, User_Registration, User_Update
 
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
-
 
 async def create_user(db: AsyncSession, user: User_Registration) -> User:
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = hash_password(user.password)
     db_user = User(
         name=user.name,
         email=user.email,
@@ -22,6 +20,11 @@ async def create_user(db: AsyncSession, user: User_Registration) -> User:
 
 async def get_user(db: AsyncSession, user_id: int) -> User_Read | None:
     result = await db.execute(select(User).where(User.id == user_id))
+    return result.scalar_one_or_none()
+
+
+async def get_user_by_email(db: AsyncSession, email: str) -> User | None:
+    result = await db.execute(select(User).where(User.email == email))
     return result.scalar_one_or_none()
 
 
