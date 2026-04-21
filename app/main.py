2803b1607187e3c0dc90b3effc508c6e3ad72422
sasha4ka <sasha4ka99991@gmail.com>
2026-04-21 22:15:06 +0300

    add cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from fastapi.security import HTTPBearer
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.users import router as users_router
 from app.api.v1.notes import router as notes_router
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 bearer_scheme = HTTPBearer(bearerFormat="JWT", description="Enter: Bearer <token>")
 
