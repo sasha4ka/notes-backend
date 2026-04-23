1be0b5516bf6a95b361b5a55d9617f4c06755cc2
sasha4ka <sasha4ka99991@gmail.com>
2026-04-23 16:58:54 +0300

    Migration: is_admin in User

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -11,3 +11,4 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     notes = relationship("Note", back_populates="author")
     is_active = Column(Boolean, nullable=False, default=True)
+    is_admin = Column(Boolean, nullable=False, default=False)
